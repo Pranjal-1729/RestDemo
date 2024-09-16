3f9c708fe9a138d1a9a31bd5bbc4f45df19a10bb
Pranjal Shinde <pranjalshinde.1729@gmail.com>
2024-09-16 19:13:09 +0530

    Rest Demo Project

---
+++ src/main/java/com/thinkconstructive/rest_demo/exception/CloudVendorExceptionController.java
@@ -0,0 +1,19 @@
+package com.thinkconstructive.rest_demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CloudVendorExceptionController {
+
+    @ExceptionHandler(value = {CloudVendorNotFoundException.class})
+    public ResponseEntity<Object> handleCloudVendorNotFoundException
+            (CloudVendorNotFoundException cloudVendorNotFoundException){
+        CloudVendorException cloudVendorException = new CloudVendorException(cloudVendorNotFoundException.getMessage(),
+                cloudVendorNotFoundException.getCause(), HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(cloudVendorException, HttpStatus.NOT_FOUND);
+    }
+}
